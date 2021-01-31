8544c813091ea3bcfbfd61591c4216b7216d3c7c
Kanhaiya Kumar <kanhaiyak42@gmail.com>
2021-01-31 11:57:55 +0530

    made changes

--- server.js
+++ server.js
@@ -1,44 +1,36 @@
 var mongo = require("mongodb").MongoClient,
-    client = require('socket.io').listen(9000).sockets;
-
+  client = require("socket.io").listen(9000).sockets;
 
 mongo.connect("mongodb://127.0.0.1/chat", function (err, db) {
-    if (err) throw err;
+  if (err) throw err;
 
+  client.on("connection", function (socket) {
+    console.log("client connected");
+    var col = db.collection("messages");
 
-    client.on('connection', function (socket) {
+    var oldMsg = col
+      .find()
+      .limit(200)
+      .toArray(function (mongoError, res) {
+        socket.emit("output", res);
+      });
+    socket.emit("status", "Connected");
 
+    //Wait for input
+    socket.on("input", function (data) {
+      var name = data.name,
+        message = data.message,
+        whitespacePattern = /^\s*$/;
 
-        console.log('client connected');
-        var col = db.collection('messages');
-
-        var oldMsg = col.find().limit(200).toArray(function (mongoError,res) {
-            socket.emit('output',res);
+      if (whitespacePattern.test(name) || whitespacePattern.test(message)) {
+        console.log("invalid");
+        socket.emit("status", "Enter A Name and Some Message");
+      } else {
+        col.insertOne({ name: name, message: message }, function () {
+          console.log("Inserted");
+          client.emit("output", [{ name: name, message: message }]);
         });
-        socket.emit('status','Connected');
-
-        //Wait for input
-        socket.on('input', function (data) {
-
-            var name = data.name,
-                message = data.message,
-                whitespacePattern = /^\s*$/;
-
-            if (whitespacePattern.test(name) || whitespacePattern.test(message)) {
-                console.log('invalid');
-                socket.emit('status', 'Enter A Name and Some Message');
-            } else {
-                col.insertOne({name: name, message: message}, function () {
-                    console.log('Inserted');
-                    client.emit('output',[{name:name,message:message}]);
-
-                })
-            }
-
-        })
-    })
-})
-
-
-
-
+      }
+    });
+  });
+});
